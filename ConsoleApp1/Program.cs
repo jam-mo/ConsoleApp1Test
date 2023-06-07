92f536496a144786a5308e1773371bcdf1666e03
Jamie <jamiemolseed@hotmail.co.uk>
2023-06-07 10:53:15 +0100

    Update Program.cs
    remove multiple messages

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C#, another location");
-Console.WriteLine("Hello, C#, another location");
-Console.WriteLine("Hello, C#, another location");
-Console.WriteLine("Hello, C#, another location");
-Console.WriteLine("Hello, C#, another location");
+Console.WriteLine("Hello, C#, another location v2");
+
